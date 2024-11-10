25e1426e1970ca053d602a2793345ade1c5a1495
JuppyGuppy <dylangotrent@gmail.com>
2024-11-09 21:14:25 -0500

    Fixed it to be not actually random as intended and is actually quite weighted

--- RandomRoller.java
+++ RandomRoller.java
@@ -9,16 +9,21 @@ public class RandomRoller
 	public static void main(String[] args) {
         System.out.println("What die size would you like to use?");
         Scanner input = new Scanner(System.in);
-        int dieSize=input.nextInt();
+        int dieSize=input.nextInt()+1;
         input.nextLine();
 		int roll=0;
 		Random rand = new Random();
         boolean again = true;
+        int highest;
         while(again){
+            highest = 0;
             for(int i=0; i<5; i++){
                 roll = rand.nextInt(dieSize);
+                if(roll>highest){
+                    highest=roll;
+                }
             }
-            System.out.println("Your roll: "+roll+", wish to roll again?(no to quit)");
+            System.out.println("Your roll: "+highest+", wish to roll again?(no to quit)");
             String response = input.nextLine();
             if("no".equalsIgnoreCase(response)){
                 again=false;
